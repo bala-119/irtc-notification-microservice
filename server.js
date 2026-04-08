3e376da1944df693d11a95a076b477e9232a1cf2
bala <bala27cs@gmail.com>
2026-04-08 17:47:35 +0530

    new updates

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./app/configs/dbConnect");   // your DB file
 const notificationRoutes = require("./app/routes/sendemailroutes");
@@ -7,6 +8,7 @@ dotenv.config();
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 // connect MongoDB
